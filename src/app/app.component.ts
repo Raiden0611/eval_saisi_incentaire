db9931214c9ffc9d3b7cf1339209f284fbc26b5f
Raiden <adrien.graindorge@my-digital-school.org>
2024-02-20 15:14:32 +0100

    debut deuxieme page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+// Désactive l'ouverture automatique du clavier virtuel pour les champs input et textarea
+/* document.addEventListener('DOMContentLoaded', function () {
+  var inputElements = document.querySelectorAll('input, textarea');
+  for (var i = 0; i < inputElements.length; i++) {
+    inputElements[i].setAttribute('readonly', 'readonly');
+  }
+}); */
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   title = 'saisie_inventaire';
   inputValue: string = '';
   inputresultValue: string = '';
+  inputTitreInventaire: string = '';
+  selectedUnit: string = '';
+  dataRows: any[] = [];
 
   updateInput(value: string) {
     this.inputValue += value; // Concatène la nouvelle valeur à la valeur existante
@@ -25,10 +38,39 @@ export class AppComponent {
     } catch (error) {
       // Gère les erreurs d'évaluation
       console.error('Erreur de calcul :', error);
+      this.inputresultValue = 'ERREUR de calcul';
     }
   }
-  enterData() { }
 
-  clearcalcul() { this.inputValue = ''; }
+  clearcalcul() { this.inputValue = ''; this.inputresultValue = ''; } // Supprime tout les caractères de inputValue
+  clearOnecalcul() {
+    this.inputValue = this.inputValue.slice(0, -1); // Supprime le dernier caractère de inputValue
+  }
+
+
+  enregistrement() {
+    console.log("Valeur de inputTitreInventaire :", this.inputTitreInventaire);
+    console.log("Valeur de inputresultValue :", this.inputresultValue);
+    console.log("Unité sélectionnée :", this.selectedUnit);
+    try {
+      const newData = {
+        nom: this.inputTitreInventaire,
+        valeur: this.inputresultValue,
+        unite: this.selectedUnit
+      };
+
+      this.dataRows.push(newData);
+
+      // Réinitialisez les valeurs après l'enregistrement
+      this.inputTitreInventaire = '';
+      this.inputValue = '';
+      this.inputresultValue = '';
+      console.log(this.dataRows)
+    } catch (e) {
+      console.error('Erreur dans l\'enregistrement :', e);
+      alert('Erreur dans l\'enregistrement')
+    }
+
+  }
 }
 
